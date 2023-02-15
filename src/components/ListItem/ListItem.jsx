2742771dffe3a803aa17d7c196172d6ac0fad3a0
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-02-15 20:58:03 +0100

    Refactor list item

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./ListItem.css";
 
-export default function ListItem({ name, price, checked, onChange }) {
+export default function ListItem({ item }) {
+  const { name, price, checked, onChange } = item;
+
   return (
     <div className="list-item-row">
       <input onChange={() => onChange()} type="checkbox" checked={checked} />
       <span
+        data-testid="item-info"
         className={`list-item-text ${checked ? "list-item-text-done" : ""}`}
       >
         {name + ", " + price + ":-"}
