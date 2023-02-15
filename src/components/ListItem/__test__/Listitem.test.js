40b7095248769f457294ff516489643f736d40d1
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-02-15 23:35:32 +0100

    Refactor ListItem test

--- src/components/ListItem/__test__/Listitem.test.js
+++ src/components/ListItem/__test__/Listitem.test.js
@@ -1,13 +1,14 @@
-import { render, screen, fireEvent, getByText } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import ListItem from "../ListItem";
 
 const mockedCheckedItem = {name: 'bed', price: '100', checked: true, onChange: () => {}};
 const mockedUncheckedItem = {name: 'bed', price: '100', checked: false, onChange: () => {}};
+const mockedOnChange = () => {};
 
-describe("input testing", () => {
+describe("ListItem testing", () => {
   it("should render pending item", async () => {
-    render(<ListItem item={mockedUncheckedItem} />);
+    render(<ListItem item={mockedUncheckedItem} onChange={mockedOnChange} />);
     const spanElement = screen.getByTestId("item-info");
     const checkboxElement = screen.getByRole("checkbox");
     expect(spanElement).toBeInTheDocument();
@@ -15,13 +16,13 @@ describe("input testing", () => {
   });
 
   it("should show a checked checkbox", async () => {
-    render(<ListItem item={mockedCheckedItem} />);
+    render(<ListItem item={mockedCheckedItem} onChange={mockedOnChange} />);
     const checkboxElement = screen.getByRole("checkbox");
     expect(checkboxElement).toBeChecked();
   });
 
   it("should show an unchecked checkbox", async () => {
-    render(<ListItem item={mockedUncheckedItem} />);
+    render(<ListItem item={mockedUncheckedItem} onChange={mockedOnChange} />);
     const checkboxElement = screen.getByRole("checkbox");
     expect(checkboxElement).not.toBeChecked();
   });
